1de7c88de5c5f91e319cecac01d6f4875f0fa366
Nithyanandan Sathiyanathan <kavinithiy@gmail.com>
2021-06-17 08:47:50 +0530

    Junction box logic done

---
+++ src/app/components/file-choose-modal/file-choose-modal.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-file-choose-modal',
+  templateUrl: './file-choose-modal.component.html',
+  styleUrls: ['./file-choose-modal.component.scss']
+})
+export class FileChooseModalComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/components/junctionbox-modal/junctionbox-modal.component.ts
+++ src/app/components/junctionbox-modal/junctionbox-modal.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { MatDialogRef } from '@angular/material/dialog';
 @Component({
   selector: 'app-junctionbox-modal',
   templateUrl: './junctionbox-modal.component.html',
@@ -10,11 +11,17 @@ export class JunctionboxModalComponent implements OnInit {
     systemName: ['', Validators.required],
     connectionFill: ['', Validators.required]
   });;
-  constructor(private formBuilder: FormBuilder,) { }
+  constructor(private formBuilder: FormBuilder, public dialogRef: MatDialogRef<JunctionboxModalComponent>) { }
   
   ngOnInit(): void {
 
 
   }
-
+  public onSubmit(): void {
+    if (this.JunctionBoxFrom.valid) {
+       console.log('junctionbox', this.JunctionBoxFrom.value);
+       this.dialogRef.close(this.JunctionBoxFrom.value);
+    }
+    
+  }
 }
