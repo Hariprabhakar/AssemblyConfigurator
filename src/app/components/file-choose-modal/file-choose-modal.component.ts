7c8d2efb2b007d5c106a2fa1cc226f14b7823b3a
Nithyanandan Sathiyanathan <kavinithiy@gmail.com>
2021-06-23 08:36:26 +0530

    File upload primary image functionality done

--- src/app/components/file-choose-modal/file-choose-modal.component.ts
+++ src/app/components/file-choose-modal/file-choose-modal.component.ts
@@ -15,10 +15,14 @@ export class FileChooseModalComponent implements OnInit {
   public showUplodingFiles: boolean;
   public filesToUpload: number;
   public tempUploads: any;
+  public showAddBtn: boolean;
+  public primaryImgNotSet: string;
+  public isPrimaryImgNotSet: boolean;
   public FileChooseFrom: FormGroup = this.formBuilder.group({
       fileupload: [''],
       items: this.formBuilder.array([]),
   });
+  public showEditPrimaryImage: boolean;
   constructor(public fileChoose: MatDialogRef < FileChooseModalComponent > , @Inject(MAT_DIALOG_DATA) public data: any, private formBuilder: FormBuilder, ) {
       this.fileObj = {
           imageList: [],
@@ -30,6 +34,11 @@ export class FileChooseModalComponent implements OnInit {
       this.showUplodingFiles = true;
       this.tempUploads = [];
       this.filesToUpload = 0;
+      this.showEditPrimaryImage = false;
+      this.showAddBtn = false;
+      this.primaryImgNotSet = '';
+      this.isPrimaryImgNotSet = false;
+
   }
 
   ngOnInit(): void {
@@ -39,6 +48,7 @@ export class FileChooseModalComponent implements OnInit {
           this.showSelectedFiles = true
           this.filesToUpload = 3 - this.data.length;
       }
+      this.updateViews();
   }
 
   get items(): FormArray {
@@ -59,14 +69,13 @@ export class FileChooseModalComponent implements OnInit {
 
 
   public async handleFileInput(event: any) {
-      console.log('this.filesToUpload Before', this.filesToUpload);
+     
       this.filesToUpload = 3 - ((this.data.length || 0) + (this.tempUploads.length || 0));
-      console.log('this.filesToUpload After', this.filesToUpload);
       this.isMaxFileError = false;
-
       const fileLength = event.target.files.length;
       const maxfileLength = 3 - ((this.data.length || 0) + (this.tempUploads.length || 0));
       if (fileLength < (maxfileLength + 1)) {
+         this.showAddBtn = true;
           this.isMaxFileError = false;
           const finalData = await this.tobase64Handler(event.target.files);
           this.tempUploads = this.tempUploads.concat(finalData);
@@ -81,6 +90,7 @@ export class FileChooseModalComponent implements OnInit {
           this.maxFileError = 'User can upload only Maximum three files';
           this.isMaxFileError = true;
       }
+
 
   }
 
@@ -113,13 +123,15 @@ export class FileChooseModalComponent implements OnInit {
 
 
   public onSubmit() {
+
       if (this.FileChooseFrom.valid) {
           let finalData: any = [];
           this.tempUploads.forEach((value: any, key: any) => {
               const obj = {
                   fileName: '',
                   fileName64Bit: '',
-                  inputText: ''
+                  inputText: '',
+                  isPrimary: false
               };
               obj.fileName = value.fileName,
                   obj.fileName64Bit = value.fileName64Bit,
@@ -131,19 +143,80 @@ export class FileChooseModalComponent implements OnInit {
           finalData = finalData.concat(this.data);
           console.log('finalvalue', finalData);
           console.log('this.data', this.data);
-          this.fileChoose.close(finalData);
-      }
+          this.data = finalData;
+          this.updateViews();
+          this.showAddBtn = false;
+          this.FileChooseFrom.reset();
+
+          const control = <FormArray>this.FileChooseFrom.controls['items'];
+          for(let i = control.length-1; i >= 0; i--) {
+            control.removeAt(i)
+          }
+          this.tempUploads = [];
+          // this.fileChoose.close(finalData);
+      } 
+
   }
 
-  public closeUploadedFiles(file: any, index: any) {
-      this.data = this.data.filter((value: any, key: any) => key !== index);
-      if (this.data.length === 0) {
-          this.showSelectedFiles = false;
-      }
+
+
+  public closeUploadedFiles(file: any, index: number) {
+      this.maxFileError = "";
+      this.data = this.data.filter((value: any, key: number) => key !== index);
+      this.updateViews();
   }
 
-  public closeTempFiles(file: any, index: any) {
-    this.tempUploads = this.tempUploads.filter((value: any, key: any) => key !== index);
+  public closeTempFiles(file: any, index: number) {
+    this.tempUploads = this.tempUploads.filter((value: any, key: number) => key !== index);
+    this.updateViews();
   }
 
+  public closeBtnClick() {
+    this.maxFileError = "";
+
+    let isPrimarySet = this.data.filter((value: any, key: number) => value.isPrimary === true);
+    isPrimarySet = isPrimarySet.length !== 0 ? true : false;
+    if (!isPrimarySet) {
+      this.primaryImgNotSet = "Please set primary image";
+      this.isPrimaryImgNotSet = true;
+    } else {
+      this.fileChoose.close(this.data);
+    }
+    this.showAddBtn = false;
+  }
+
+  public updateViews(){
+    let isPrimarySet = this.data.filter((value: any, key: number) => value.isPrimary === true);
+    isPrimarySet = isPrimarySet.length !== 0 ? true : false;
+    if(this.data.length === 1) {
+      this.showSelectedFiles = true;
+      this.data[0].isPrimary = true;
+      this.showEditPrimaryImage = false;
+    } else if (this.data.length !== 0) {
+      this.showSelectedFiles = true;
+      this.showEditPrimaryImage = isPrimarySet ? false : true;
+    } else {
+      this.showEditPrimaryImage = false;
+    }
+  }
+
+  public radioChange(index:number) {
+    this.data.forEach((value: any, key: any) => {
+        if (index === key) {
+          this.data[key].isPrimary = true;
+        } else {
+          this.data[key].isPrimary = false;
+        }
+    });
+    this.showEditPrimaryImage = false;
+    this.isPrimaryImgNotSet = false;
+  }
+
+  public chnagePrimaryBtn() {
+    this.data.forEach((value: any, key: any) => {
+      this.data[key].isPrimary = false;
+    });
+    this.showEditPrimaryImage = true;
+  }
+ 
 }
