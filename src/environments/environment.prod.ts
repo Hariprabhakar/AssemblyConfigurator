36f71631f8e44fa705f20bca5785ddee76de98d9
tbalakpm <software.balu@gmail.com>
2021-07-23 11:40:25 +0530

    Deployment changes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,6 @@ export const environment = {
   production: true,
   // url: 'http://localhost:4200/assets/mock/',
   url: 'https://sit.sanveo.net/assemblyconfigurator-api/',
+  imageUrl: 'https://sit.sanveo.net/assemblyconfigurator-api/',
   mockEnabled: false
 };
